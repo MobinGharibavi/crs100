c1129be8e2ace84a5a08ae7cbcf3b0908cbe3175
mobin.2006 <mobingharibavi@gmail.com>
2024-04-13 20:35:31 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        km_bold: "KM-bold",
+        km_thin: "KM-thin",
+        km_black: "KM-black",
+      },
+    },
+  },
+  plugins: [],
+}
+
